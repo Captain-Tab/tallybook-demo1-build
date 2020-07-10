0b44a822c61e9f94707e8feb2f751c91c5ba5ff2
Tab <d491405879@gmail.com>
2020-07-10 22:35:15 +0800

    deploy

---
+++ precache-manifest.5bfadd3053912926be8564480fa49ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4e20b0eb4e289472c28487701ee170",
+    "url": "/tallybook-demo1-build/QRcode.png"
+  },
+  {
+    "revision": "10ba0d7c9d82e867837d",
+    "url": "/tallybook-demo1-build/css/app.44932a37.css"
+  },
+  {
+    "revision": "ff5185e90bfae7badebd6bab96a3213a",
+    "url": "/tallybook-demo1-build/index.html"
+  },
+  {
+    "revision": "10ba0d7c9d82e867837d",
+    "url": "/tallybook-demo1-build/js/app.e556b104.js"
+  },
+  {
+    "revision": "652af8dcff198254390f",
+    "url": "/tallybook-demo1-build/js/chunk-vendors.c13f2faf.js"
+  },
+  {
+    "revision": "916f48456d3ffa656dd570f32c296aa0",
+    "url": "/tallybook-demo1-build/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tallybook-demo1-build/robots.txt"
+  }
+]);
